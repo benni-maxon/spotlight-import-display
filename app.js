a2bde6ce279f9a21898133ea6707a1e802c9c33c
benni maxon <benni.maxon@gmail.com>
2022-11-17 13:09:52 -0800

    created window event listener in app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 /* Imports */
 
+import { getMovies } from './fetch-utils.js';
+import { renderMovieTemplate } from './render-utils.js';
+
 /* Get DOM Elements */
+const movieDivContainer = document.getElementById('movie-div-container');
 
 /* State */
 
 /* Events */
 
 /* Display Functions */
-
+window.addEventListener('load', async () => {
+    const movies = await getMovies();
+    for (let movie of movies) {
+        const movieEl = renderMovieTemplate(movie);
+        movieDivContainer.append(movieEl);
+    }
+});
 // (don't forget to call any display functions you want to run on page load!)
